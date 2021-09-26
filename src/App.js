204e2685d15ba6680183a7b4a30dd247b1bda26c
hannalampinen <n0laha00@students.oamk.fi>
2021-09-26 18:40:31 +0300

    list tehtävä

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react'
+import uuid from 'react-uuid';
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const names = ['Hanna', 'Maria', 'Lampinen', 'Hanna']
+    setItems(names);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{margin: '30px'}}>
+      <h3>Names</h3>
+      <ul>
+        {items.map(item => (
+          <li key={uuid()}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
